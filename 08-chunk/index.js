d6b5a5a1c1d5b400a2fddf508a9021806c1ba03c
Taras-Rm <tarasrom25@gmail.com>
2021-09-09 19:06:26 +0300

    08-chunk

--- 08-chunk/index.js
+++ 08-chunk/index.js
@@ -1,5 +1,20 @@
 // TODO: rename function, maybe array-of-arrays
 
 export const chunk = (arr = [], size = 0) => {
-
-};
+    let arrCopy = arr;
+    let newArr = [];
+    let subArr = [];
+    
+    
+    while(arrCopy.length) {
+      newArr.push(arrCopy.slice(0, size));
+      
+      for(let i = 0; i < size; i++) {
+        arrCopy.shift();
+      }
+    }
+  
+    return newArr;
+  
+  };
+  
